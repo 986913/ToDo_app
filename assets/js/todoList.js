4e92f444b915f39cb450f1b9c8bcca24b96c9064
mingyue liu <MLIU2@LTU.EDU>
2017-12-04 19:02:40 -0500

    add all files

---
+++ assets/js/todoList.js
@@ -0,0 +1,59 @@
+//check off specific Todos by clicking
+// method 1:
+// $("li").click(function(){
+// 	// if li is gray
+// 	if ($(this).css("color")==="rgb(128, 128, 128)")
+// 	{
+// 		// turn it black
+// 		$(this).css({
+// 			color:"black",
+// 			textDecoration:"none"
+// 					})
+// 	}		
+// 	// if li is black
+// 	else
+// 	{   // turn it gray
+// 		$(this).css({
+// 		color:"gray",
+// 		textDecoration:"line-through"
+// 				})
+// 	}
+// }) 
+// method 2:
+$("ul").on("click","li",(function(){
+	$(this).toggleClass("completed");// 其中completed这个class在css文件中定义的
+}))
+
+
+
+//Click on X to delete todo
+//注意： 这个例子span镶嵌在li中，所以要防止bubble up的问题产生 
+$("ul").on("click","span",(function(event){
+	event.stopPropagation();//stopPropagation()is a jQuery method that will stop the event from bubbling up.
+	$(this).parent().fadeOut("10000",function(){
+		$(this).remove();
+	})
+}))
+
+//click()only adds listeners for existing elements
+//on() will add listeners for all potential future elements
+$("input[type='text']").keypress(function(event){
+	if(event.which===13)
+	{	// grabbing new todo text from input
+		var todoText=$(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> "+ todoText + "</li>");
+
+	}
+})
+//.append()    it can take a string of html and it will then append those elements to whatever we selected
+
+//$("ul").on("click","span",(function(event){...
+//$("ul").on("click","li",(function(){.....
+// above two's scenod argument : span   li     that means when i click any span and li inside the ul. the function will be executed.
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
+
